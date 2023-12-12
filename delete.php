7933b0d7dbedd50139498933170b8f1a72c7c354
NuriyaMaulidah <135193833+NuriyaMaulidah@users.noreply.github.com>
2023-12-12 15:34:06 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,13 +1,13 @@
 <?php
 require '../../koneksi.php';
 
-  $id = $_GET["kode_dokter"];
+  $id = $_GET["No_RM"];
 
-  $hapus = mysqli_query($link, "DELETE FROM data_dokter WHERE kode_dokter=$id");
+  $hapus = mysqli_query($link, "DELETE FROM data_pasien WHERE No_RM='".$id."'");
 
   if($hapus){
-    echo "<script> alert('Berhasil menghapus data !'); document.location.href='tampil.php'; </script>";
-  }else{
-    "<script> alert('Gagal nebghapus data !'); document.location.href='tampil.php'; </script>";
+    echo "<script> alert('Berhasil menghapus data !'); document.location.href='tampilpasien.php'; </script>";
+  } else{
+    echo "<script> alert('Gagal menghapus data !'); document.location.href='tampilpasien.php'; </script>";
   }
 ?>
